f70dd30105ba13875d21d474e786a01bc8967c38
Naufal Ghifary <naufal.m.g.54@gmail.com>
2025-03-04 14:49:00 +0700

    deploy vercel

--- src/libs/prisma-client.ts
+++ src/libs/prisma-client.ts
@@ -2,7 +2,9 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = new PrismaClient({
-  datasources: { db: { url: process.env.DATABASE_URL } }
-})
+export const prisma =
+globalForPrisma.prisma || new PrismaClient()
+// const prisma = new PrismaClient({
+//   datasources: { db: { url: process.env.DATABASE_URL } }
+// })
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
